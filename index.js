0516fd109c701c249f56f305a6db178e31104dc8
justin-aquino <justin.aquino25@gmail.com>
2022-03-25 23:45:51 -0700

    pushing changes

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require('./models')
 const express = require('express')
 const app = express()
 const cors = require('cors')
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 8000
 
 //middleware
 app.use(express.json())
