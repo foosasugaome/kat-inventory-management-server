2efc3c46b94f94481b748fd918925a1f3b3b8055
Norman Teodoro <normanteodoro@hotmail.com>
2022-03-30 17:04:26 -0700

    created middleware to create admin account

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 require('./models')
 const express = require('express')
 const app = express()
+const bcrypt = require('bcrypt')
+const db = require('./models')
 const cors = require('cors')
 const morgan = require('morgan')
 const PORT = process.env.PORT || 3001
@@ -12,14 +14,41 @@ app.use(express.json())
 app.use(morgan('dev'))
 
 
-// const myMiddleWare = (req, res, next) => {
-//     console.log(`Request made  at : ${req.url}`)        
-//     next()
-// }
+const myMiddleWare = async (req, res, next) => {
+    try {
+        const adminEmail = 'admin@katims.com'
+        const adminUsername = 'admin'
+        const adminPassword = 'admin'
 
-// app.use(myMiddleWare)
+        // check if admin account exists
+        const userCheck = await db.Users.findOne({ $or: [
+            {email: adminEmail,},{username : adminUsername }]
+        })
 
-app.get('/', (req,res)=>{
+        // if(userCheck) return res.status(409).json({ msg: 'Email/Username already in use.'})
+        if(!userCheck) {
+            const salt = 12
+        const hashedPassword = await bcrypt.hash(adminPassword, salt)
+        
+        // create a user in the db
+        const newUser = await db.Users.create({
+            username: adminUsername,
+            firstname: 'admin',
+            lastname: 'admin',
+            email: adminEmail,
+            password: hashedPassword,
+            manager: true
+        })        
+        }        
+    } catch(err) {
+        console.log(err)        
+    }    
+    next()
+}
+
+app.use(myMiddleWare)
+
+app.get('/', myMiddleWare, (req,res)=>{
     res.json({msg : `KAT Software Solutions`})
 })
 
